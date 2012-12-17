45af3caec82210fca9694d5ac994a1cc490ea0a8
ting <ting@ting-ThinkPad-T400.(none)>
2012-12-17 13:25:03 +0800

    Update composer. Add depended vendor lib. Add Product controller.

--- module/Application/config/module.config.php
+++ module/Application/config/module.config.php
@@ -69,7 +69,8 @@ return array(
     ),
     'controllers' => array(
         'invokables' => array(
-            'Application\Controller\Index' => 'Application\Controller\IndexController'
+            'Application\Controller\Index' => 'Application\Controller\IndexController',
+            'Product\Controller\Index' => 'Product\Controller\IndexController',
         ),
     ),
     'view_manager' => array(
