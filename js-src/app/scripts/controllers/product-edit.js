55e5cb7b8647b04ca5eaef541bf80d856b5d007c
Ting Wang <tting.wang@gmail.com>
2012-12-24 08:59:48 +0800

    After saving new product change interface to edit mode.

--- js-src/app/scripts/controllers/product-edit.js
+++ js-src/app/scripts/controllers/product-edit.js
@@ -1,15 +1,20 @@
 'use strict';
 
-jsSrcApp.controller('Product-EditCtrl', ['$scope', 'product', function($scope, product) {
+jsSrcApp.controller(
+    'Product-EditCtrl', ['$scope', 'product', function($scope, product) {
     var newProduct = {};
+    $scope.title = 'Add product';
     $scope.product = newProduct;
     var Product = product.Product;
+    var scope = $scope;
     $scope.save = function() {
-        var p = Product.get();
+        var p = new Product;
         p.name = newProduct.name;
         p.time = newProduct.time;
         p.price = newProduct.price;
-        p.$save();
-    }
-    }
+        p.$save(function(p) {
+            scope.title = 'Edit: ' + p.name;
+        });
+    };
+}
 ]);
--- js-src/app/scripts/controllers/product.js
+++ js-src/app/scripts/controllers/product.js
@@ -1,6 +1,6 @@
 'use strict';
 
-jsSrcApp.controller('ProductCtrl', ['$scope', 'product', function($scope, product) {
+jsSrcApp.controller(
+    'ProductCtrl', ['$scope', 'product', function($scope, product) {
     $scope.products = product.Product.query();
-    $scope.product = Product.get({id: 69});
 }]);
