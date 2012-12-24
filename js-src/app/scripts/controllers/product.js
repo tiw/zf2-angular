287d2195e138e36953eac338b6fb8f7266abb27b
Ting Wang <tting.wang@gmail.com>
2012-12-24 11:02:47 +0800

    remove conflict.

--- js-src/app/scripts/controllers/product.js
+++ js-src/app/scripts/controllers/product.js
@@ -1,15 +1,10 @@
 'use strict';
 
-<<<<<<< HEAD
-jsSrcApp.controller('ProductCtrl', ['$scope', '$routeParams', 'product', function($scope, $routeParams, product) {
-    // $scope.products = product.Product.query();
+jsSrcApp.controller(
+    'ProductCtrl',
+    ['$scope', '$routeParams', 'product',
+        function($scope, $routeParams, product) {
     var p = product.Product;
     console.log(p);
     $scope.product = p.get({id: $routeParams.id});
-    
-=======
-jsSrcApp.controller(
-    'ProductCtrl', ['$scope', 'product', function($scope, product) {
-    $scope.products = product.Product.query();
->>>>>>> 602c64d6e3b08b1fd3b47b887ac9a758555e8d42
 }]);
